d77746994db7321b9d94f9993f93f5035a5c803f
penny-ctrl <948268007@qq.com>
2020-06-23 19:57:42 +0800

    第二次提交

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 $("#footer").load("html/footer.html");
+$(function () {
+  $(".nav ul li ").hover(
+    function () {
+      $(this).css("color", "orange");
+      // over
+      console.log($(this));
+    },
+    function () {
+      $(this).css("color", "black");
+      // out
+    }
+  );
+});
